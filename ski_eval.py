892cb87f1953b471bf3aa928a9efd4ac0b1205a3
Grace Jin <gracejxq@myth57.stanford.edu>
2025-10-02 22:54:34 -0700

    done

--- ski_eval.py
+++ ski_eval.py
@@ -6,6 +6,27 @@ import src.ski as ski
 # TASK: Implement the below function `eval`.
     
 def eval(e: ski.Expr) -> ski.Expr:
-    # BEGIN_YOUR_CODE
-    raise NotImplementedError()
-    # END_YOUR_CODE
+    if isinstance(e, ski.App):
+        e1 = eval(e.e1)
+        # Don't greedily evaluate e2 in case it infinite loops
+
+        # I x with format e1 e2
+        if isinstance(e1, ski.I):
+            return eval(e.e2)
+
+        # (K x) y with format (e1.e1 e1.e2) e2
+        if isinstance(e1, ski.App) and isinstance(e1.e1, ski.K):
+            return eval(e1.e2)
+
+        # ((S x) y) z with format ((e1.e1.e1 e1.e1.e2) e1.e2) e2
+        if isinstance(e1, ski.App) and isinstance(e1.e1, ski.App) and isinstance(e1.e1.e1, ski.S): 
+            x = e1.e1.e2
+            y = e1.e2
+            z = eval(e.e2)
+            return eval(ski.App(ski.App(x, z), ski.App(y, z)))
+
+        else: 
+            return ski.App(e1, eval(e.e2))
+
+    else: # can't do anything if there's no application
+        return e
